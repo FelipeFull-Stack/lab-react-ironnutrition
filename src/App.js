0f3b5bc7e710d96aff676c2db7bf2959f6ea3d43
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-11-10 23:35:32 -0300

    RESOLVI UM BUG GIGANTE - apaguei a pasta foods.json 2x cla... perdi tudo 1x sem salvar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import foods from "./foods.json";
+import { useState } from "react";
+import global from "./global.css";
 
 function App() {
+
+  const [food, setFood] = useState("");
+
+
+
   return (
-    <h1>Oi</h1>
+    <div>
+      {foods.map((currentElement) => <p>{currentElement.name}
+        <img src={currentElement.image} alt="" key={currentElement.name}/>
+      </p>)}
+    </div>
   );
 }
 
